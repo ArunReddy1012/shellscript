b2d000b28f7f7814f729a73e3f070ac2d261a020
arun <arunreddyetta@gmail.com>
2023-07-01 12:02:19 +0530

    made changes

---
+++ functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#checking for root user permissions
+if [ uid -ne 0]
+then
+    echo "Please run a a sudo user"
+    exit 1
+fi
+
+#validation function block
+validate () {
+    if [ $1 -ne 0 ]
+    then
+        echo "Installation of $2 is failed"
+        exit 2
+    else
+        echo " Installation of $2 is successfull"
+    fi
+}
+
+#calling a function and passing parameters
+validate $? Git
+yum install git -y
+
+validate $? wget
+yum install wget -y
